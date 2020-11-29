67e3c2800b2a6cf798c3589f8ff54bdb9ce42e83
AnjiEasonChan <50496749+AnjiEasonChan@users.noreply.github.com>
2020-11-29 14:49:54 +0800

    Create 两数相加.py

---
+++ 两数相加/两数相加.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        prev = result = ListNode(None)
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            prev.next = ListNode(carry % 10)
+            prev = prev.next
+            carry //= 10
+        return result.next
