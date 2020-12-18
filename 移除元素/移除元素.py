e49f6600f0c98e91bdc5f754b9997d5d7576f774
AnjiEasonChan <50496749+AnjiEasonChan@users.noreply.github.com>
2020-12-18 21:06:55 +0800

    Create 移除元素.py

---
+++ 移除元素/移除元素.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        a = 0
+        b = 0
+
+        while a < len(nums):
+            if nums[a] != val:
+                nums[b] = nums[a]
+                b += 1
+            a += 1
+
+        return b
