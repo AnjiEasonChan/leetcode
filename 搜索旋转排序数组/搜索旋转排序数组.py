850ec4193e9c5aa0b06d095a9a18e0a8214092e5
AnjiEasonChan <50496749+AnjiEasonChan@users.noreply.github.com>
2020-12-18 21:14:32 +0800

    Create 搜索旋转排序数组.py

---
+++ 搜索旋转排序数组/搜索旋转排序数组.py
@@ -0,0 +1,24 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            
+            # 左半段有序
+            if nums[mid] >= nums[left]:
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            # 右半段有序
+            else:
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
